6995f62f7607bedd6fb3dac0058d962205f8d25a
mohammad rahimaee <mohammad.rahimaee@gmail.com>
2021-12-02 21:22:48 +0330

    fix password_reset_confirm.html update urls.py

--- mytag_account/urls.py
+++ mytag_account/urls.py
@@ -8,5 +8,17 @@ app_name = 'account'
 urlpatterns = [
     path('login', user_login_page, name='login'),
     path('register', user_register_page, name='register'),
-    path('activate/<uidb64>/<token>', activate, name='activate'),
+    path('account/activate/<uidb64>/<token>', activate, name='activate'),
+    path('account/password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='mytag_account/password_reset_form.html'),
+         name='password_reset'),
+    path('account/password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='mytag_account/password_reset_done.html'),
+         name='password_reset_done'),
+    path('account/reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="mytag_account/password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path('account/reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='mytag_account/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
